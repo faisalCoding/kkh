675d5e9923b4ce8006cecc4ad4d817dc93417949
faisal anaya <faisalbakhsh07@gmail.com>
2021-07-16 13:56:52 +0300

    add logo

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -22,7 +22,7 @@ class CreateNewUser implements CreatesNewUsers
     {
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
-            'phone' => ['required', 'regex:/(05)[0-9]{8}/'],
+            'phone' => ['required', 'regex:/(05)[0-9]{8}/', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
