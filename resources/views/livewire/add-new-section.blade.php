499e298ca196b05f399009591f1036072877ddc0
Omar bakhsh <40505899+omar-bakhsh@users.noreply.github.com>
2021-07-27 18:00:10 +0300

    Update add-new-section.blade.php

--- resources/views/livewire/add-new-section.blade.php
+++ resources/views/livewire/add-new-section.blade.php
@@ -3,7 +3,7 @@
 
         <h1>اضافة قسم جديد</h1>
         <div class="">
-            <h1 class="text-gray-800 text-xl py-3 ">اسم القسم</h1>
+            <h1 class="text-gray-800 text-xl py-3 ">__("Department Name")</h1>
             <input class="bg-gray-100 rounded-md h-14 border-none w-96" type="text" wire:model="new_section_name">
             @error('new_section_name')
                 <span class=" text-red-500">{{ $message }}</span>
@@ -11,7 +11,7 @@
         </div>
 
         <div class="">
-            <h1 class="text-gray-800 text-xl py-3 ">وصف القسم</h1>
+            <h1 class="text-gray-800 text-xl py-3 ">__("Section description")</h1>
 
             <textarea class="bg-gray-100 rounded-md h-14 border-none w-96" type="text"
                 wire:model="new_section_description"></textarea>
@@ -21,7 +21,7 @@
         </div>
 
         <div class="">
-            <h1 class="text-gray-800 text-xl py-3 ">مدير القسم</h1>
+            <h1 class="text-gray-800 text-xl py-3 ">__("Section Manager")</h1>
 
             <select wire:model="new_scetion_manager">
                 @foreach (App\Models\SectionManager::get() as $section_managet)
@@ -34,14 +34,14 @@
         </div>
 
         <div class="flex flex-col">
-            <h1 class="text-gray-800 text-xl py-3 ">صورة القسم</h1>
+            <h1 class="text-gray-800 text-xl py-3 ">__("Section picture")</h1>
 
             <form class="h-96" wire:submit.prevent="saveFile" class="flex flex-col">
                 <input type="file" wire:model="file" class="custom-file-input">
-                <div wire:loading wire:target="file" class="text-xl text-yellow-500 block">جاري تحميل الصورة</div>
+                <div wire:loading wire:target="file" class="text-xl text-yellow-500 block">__("Loading image")</div>
                 @if ($file)
-                <div class="py-3 text-lg text-gray-700 ">اسم الصورة : {{ $new_section_image_name }}</div>
-                <div class="py-3 text-lg text-gray-700 ">معاينة الصورة</div>
+                <div class="py-3 text-lg text-gray-700 ">__("image name:") {{ $new_section_image_name }}</div>
+                <div class="py-3 text-lg text-gray-700 ">__("Image preview")</div>
                 
                 <img class="max-h-40" src="{{ $file->temporaryUrl() }}">
                 @endif
@@ -53,7 +53,7 @@
         <div class="py-2">
             <button
                 class="rounded-md w-52 h-14 text-white font-bold flex justify-center items-center bg-main-grd-button"
-                wire:click="addNewSection">اضف</button>
+                wire:click="addNewSection">__("add")</button>
         </div>
 
     </div>
